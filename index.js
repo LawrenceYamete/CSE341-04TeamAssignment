23edf2683ea7a6ba6c4b7723d73f80ce04956482
LawrenceYamete <LawrenceYamete>
2023-05-09 03:46:19 +0800

    update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(cors())
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
